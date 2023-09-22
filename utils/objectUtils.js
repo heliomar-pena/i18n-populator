75e3fbbc5cd35e99620ed4c65121b28ba593bdea
victor-heliomar <victordev2002@gmail.com>
2023-09-22 18:38:15 -0400

    supporting falsy values on hasProperty function

--- utils/objectUtils.js
+++ utils/objectUtils.js
@@ -1,8 +1,8 @@
-const has = (obj, path) => {
+const hasProperty = (obj, path) => {
   // Regex explained: https://regexr.com/58j0k
   const pathArray = Array.isArray(path) ? path : path.match(/([^[.\]])+/g)
 
-  return !!pathArray.reduce((prevObj, key) => prevObj && prevObj[key], obj)
+  return pathArray.reduce((prevObj, key) => prevObj && prevObj[key], obj) !== undefined;
 }
 
-module.exports = { has };
+module.exports = { hasProperty };
