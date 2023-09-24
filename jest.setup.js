433fc1ad5426b16913568fad985383ef2ddb4d70
victor-heliomar <victordev2002@gmail.com>
2023-09-24 18:38:42 -0400

    Added unit test for overwriting prompt feature

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,12 @@
 jest.mock('@vitalets/google-translate-api', () => ({
   translate: jest.fn((text, {from, to}) => ({ text: `${text} translated from ${from} to ${to}` })),
 }));
+
+jest.mock(
+  'prompt-sync',
+  () => {
+    const mPrompt = jest.fn(() => 'yes');
+    return jest.fn(() => mPrompt);
+  },
+  { virtual: true },
+);
