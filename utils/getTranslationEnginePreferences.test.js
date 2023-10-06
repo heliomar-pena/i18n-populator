683a4595e7d95a2bd769717fa999dece8ed86d91
victor-heliomar <victordev2002@gmail.com>
2023-10-06 18:49:36 -0400

    Added tests to getTranslationEnginePreferences function

---
+++ utils/getTranslationEnginePreferences.test.js
@@ -0,0 +1,43 @@
+const {
+  getTranslationEnginesToUse,
+  DEFAULT_ENGINES,
+} = require("../utils/getTranslationEnginePreferences");
+
+describe("getTranslationEnginesToUse", () => {
+  it("should return an array with the cliArgEngine if it is provided", () => {
+    const cliArgEngine = "google";
+    const result = getTranslationEnginesToUse({ cliArgEngine });
+    expect(result).toEqual(["google"]);
+  });
+
+  it("should return an array with the settingsTranslationEngines and the cliArgEngine if both are provided. cliArgEngine have more priority", () => {
+    const settingsTranslationEngines = ["google", "bing"];
+    const cliArgEngine = "yandex";
+
+    const result = getTranslationEnginesToUse({
+      settingsTranslationEngines,
+      cliArgEngine,
+    });
+    expect(result).toEqual(["yandex", "google", "bing"]);
+  });
+
+  it("should return an array with the settingsTranslationEngines if cliArgEngine is not provided", () => {
+    const settingsTranslationEngines = ["google", "bing"];
+    const cliArgEngine = null;
+    const result = getTranslationEnginesToUse({
+      settingsTranslationEngines,
+      cliArgEngine,
+    });
+    expect(result).toEqual(["google", "bing"]);
+  });
+
+  it("should return an array with the DEFAULT_ENGINES if neither cliArgEngine nor settingsTranslationEngines are provided", () => {
+    const settingsTranslationEngines = null;
+    const cliArgEngine = null;
+    const result = getTranslationEnginesToUse({
+      settingsTranslationEngines,
+      cliArgEngine,
+    });
+    expect(result).toEqual(DEFAULT_ENGINES);
+  });
+});
