ef12e5cd2e2b556bd96bdb0baff32d19cae71ba0
victor-heliomar <victordev2002@gmail.com>
2023-10-07 13:38:27 -0400

    extending docs

--- utils/getTranslationEnginePreferences.js
+++ utils/getTranslationEnginePreferences.js
@@ -3,6 +3,13 @@
  */
 const DEFAULT_ENGINES = ['google', 'bing'];
 
+/**
+ * Returns an array of translation engines to use based on the provided settings and CLI arguments. If no one is provided then the default engines are returned.
+ * @param {Object} options - The options object.
+ * @param {Array} options.settingsTranslationEngines - The array of translation engines specified in the settings file.
+ * @param {string} options.cliArgEngine - The translation engine specified as a CLI argument.
+ * @returns {Array} - The array of translation engines to use.
+ */
 const getTranslationEnginesToUse = ({ settingsTranslationEngines, cliArgEngine }) => {
     const translationEnginesToUse = [];
 
