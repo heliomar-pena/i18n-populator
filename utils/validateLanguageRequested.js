626f9fb3af52962bf55e1f0de3647da76a89c677
victor-heliomar <victordev2002@gmail.com>
2023-11-17 16:51:05 -0400

    fixed translateController tests

--- utils/validateLanguageRequested.js
+++ utils/validateLanguageRequested.js
@@ -4,19 +4,19 @@ const { getSupportedLanguagesWithNames } = require('./formatSupportedLanguages')
 
 const validateLanguageRequested = (requestedLanguage) => {
     try {
-        if (!requestedLanguage) throw new Error('No language provided.');
+        if (!requestedLanguage) throw new Error('No language provided');
         
         const isLanguageSupported = supportedLanguages[requestedLanguage] !== undefined;
         
-        if (!isLanguageSupported) throw new Error(`Language ${requestedLanguage} is not supported.`);
+        if (!isLanguageSupported) throw new Error(`Language ${requestedLanguage} is not supported`);
 
         const isLanguageSupportedByAlmostOneEngine = Object.keys(supportedLanguages[requestedLanguage]).some(value => validEngines.includes(value));
 
-        if (!isLanguageSupportedByAlmostOneEngine) throw new Error(`Language ${requestedLanguage} is not supported by any engine.`);
+        if (!isLanguageSupportedByAlmostOneEngine) throw new Error(`Language ${requestedLanguage} is not supported by any engine`);
 
         return true;
     } catch (error) {
-        throw new Error(`${error.message}. \n\nPlease use one of these:\n\n${getSupportedLanguagesWithNames(supportedLanguages).join('\n')}`)
+        throw new Error(`${error.message}.\n\nPlease use one of these:\n\n${getSupportedLanguagesWithNames(supportedLanguages).join('\n')}`)
     }
 }
 
