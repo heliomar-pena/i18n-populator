6006887ea5216d3197e94fec2652e4061677b4d1
victor-heliomar <victordev2002@gmail.com>
2023-11-17 17:09:06 -0400

    Fix formatSupportedLanguages to handle null or undefined values

--- utils/formatSupportedLanguages.js
+++ utils/formatSupportedLanguages.js
@@ -1,6 +1,6 @@
 const getSupportedLanguagesWithNames = (supportedLanguages) => {
     return Object.entries(supportedLanguages).map(([language, data]) => {
-        return `${language} -> ${data.name}`
+        return `${language} -> ${data?.name || "Unknown"}`
     })
 }
 
