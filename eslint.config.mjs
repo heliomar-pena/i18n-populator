3a03ccb45ad5ae2b7b70ce720262b14edfa81a1d
Victor Peña <victordev2002@gmail.com>
2024-06-05 20:40:18 -0400

    chore: added eslintConfigPrettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
@@ -12,4 +13,5 @@ export default [
     },
   },
   pluginJs.configs.recommended,
+  eslintConfigPrettier
 ];
