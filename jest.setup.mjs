99e2b9535703acd5827b20f0f39e798827db1cc1
Heliomar Peña <penaheliomar@gmail.com>
2024-06-22 17:43:55 -0400

    fix: fixed getOrCreateJSONFile

--- jest.setup.mjs
+++ jest.setup.mjs
@@ -10,6 +10,15 @@ jest.unstable_mockModule("./utils/promptUser.js", () => {
   return { default: jest.fn(() => "yes") };
 });
 
+jest.unstable_mockModule("fs", () => ({
+  default: {
+    existsSync: jest.fn(() => false),
+    writeFileSync: jest.fn(),
+    mkdirSync: jest.fn(),
+    rmSync: jest.fn(),
+  },
+}));
+
 jest.unstable_mockModule("bing-translate-api", () => ({
   translate: jest.fn((text, from, to) => ({
     translation: `${text} translated from ${from} to ${to} using bing`,
