aae5966ca526a0b381100f6d135630d693fe6690
Jose Tovar <jtovar26455462@gmail.com>
2025-01-23 16:51:16 -0400

    fix: check if SyntaxError was found

--- utils/importJSONFile.js
+++ utils/importJSONFile.js
@@ -16,16 +16,23 @@ async function importJSONFile(filePath, url) {
   let finalPath = "";
 
   if (typeof url === "string" && url.length > 0)
-    finalPath = platform === 'win32' ?
-      url.split("/").slice(0, -1).join("/").replace(/^file:\/\/\/?/, '') :
-      url.split("/").slice(0, -1).join("/").replace("file://", "");
+    finalPath =
+      platform === "win32"
+        ? url
+            .split("/")
+            .slice(0, -1)
+            .join("/")
+            .replace(/^file:\/\/\/?/, "")
+        : url.split("/").slice(0, -1).join("/").replace("file://", "");
 
   finalPath = path.join(finalPath, filePath);
 
   try {
     const data = await readFile(finalPath, "utf8");
     return JSON.parse(data);
-  } catch (err) {
+  } catch (error) {
+    if (error instanceof SyntaxError)
+      throw new SyntaxError("File is not a valid JSON.");
     throw new Error(`File not found: ${finalPath}`);
   }
 }
