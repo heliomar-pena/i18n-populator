e71330edd6db62d3623e7652ccc9d3ac6e3cc822
Jose Tovar <jtovar26455462@gmail.com>
2025-01-28 12:19:13 -0400

    test: improve style with Given-When-Then

--- utils/normalizeFilePath.test.js
+++ utils/normalizeFilePath.test.js
@@ -7,41 +7,63 @@ describe("Normalize file path tests", () => {
     originalPlatform = process.platform;
   });
 
-  afterAll(() => {
+  afterEach(() => {
+    // Restore original platform
     Object.defineProperty(process, "platform", {
       value: originalPlatform,
     });
   });
-
-  it("should normalize file path for Windows", () => {
-    Object.defineProperty(process, "platform", {
-      value: "win32",
+  describe("Given a Windows Environment", () => {
+    beforeEach(() => {
+      // Simulate Windows environment
+      Object.defineProperty(process, "platform", {
+        value: "win32",
+      });
     });
 
-    const filePath =
-      "file:///C:/Users/pepe/Documents/jobus/blazer/misc/i18n-populator/test-configs/test-config.json";
-    const expectedPath =
-      "C:/Users/pepe/Documents/jobus/blazer/misc/i18n-populator/test-configs";
-    expect(normalizeFilePath(filePath)).toBe(expectedPath);
-  });
+    describe("When file path is provided", () => {
 
-  it("should normalize file path for Unix", () => {
-    Object.defineProperty(process, "platform", {
-      value: "linux",
+      it("should normalize file path", () => {
+        const filePath =
+          "file:///C:/Users/pepe/Documents/jobus/blazer/misc/i18n-populator/test-configs/test-config.json";
+        const expectedPath =
+          "C:/Users/pepe/Documents/jobus/blazer/misc/i18n-populator/test-configs";
+        expect(normalizeFilePath(filePath)).toBe(expectedPath);
+      });
+    })
+  })
+
+  describe("Given a Unix-like system", () => {
+    beforeEach(() => {
+      // Simulate Unix environment
+      Object.defineProperty(process, "platform", {
+        value: "linux",
+      });
     });
 
-    const filePath =
-      "file://home/pepe/Documents/jobus/blazer/misc/i18n-populator/test-configs/test-config.json";
-    const expectedPath =
-      "home/pepe/Documents/jobus/blazer/misc/i18n-populator/test-configs";
-    expect(normalizeFilePath(filePath)).toBe(expectedPath);
-  });
+    describe("When File Path is provided", () => {
 
-  it("should throw an error if no string is provided", () => {
-    expect(() => normalizeFilePath("")).toThrow("No string provided");
-  });
+      it("should normalize file path", () => {
+        const filePath =
+          "file://home/pepe/Documents/jobus/blazer/misc/i18n-populator/test-configs/test-config.json";
+        const expectedPath =
+          "home/pepe/Documents/jobus/blazer/misc/i18n-populator/test-configs";
+        expect(normalizeFilePath(filePath)).toBe(expectedPath);
+      });
+    })
+  })
 
-  it("should handle undefined input gracefully", () => {
-    expect(() => normalizeFilePath(undefined)).toThrow("No string provided");
-  });
+  describe("Given any system", () => {
+
+    describe("When File Path is not valid", () => {
+
+      it("should throw an error if no string is provided", () => {
+        expect(() => normalizeFilePath("")).toThrow("No string provided");
+      });
+    
+      it("should handle undefined input gracefully", () => {
+        expect(() => normalizeFilePath(undefined)).toThrow("No string provided");
+      });
+    })
+  })
 });
