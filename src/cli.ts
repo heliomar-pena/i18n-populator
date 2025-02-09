88225f89101f329890b151fb52a4f35286466557
Heliomar Pena <penaheliomar@gmail.com>
2025-02-09 07:04:15 -0300

    feat: migrated getOrCreateJsonFile tests

--- src/cli.ts
+++ src/cli.ts
@@ -16,33 +16,33 @@ program
 program
   .command("translate")
   .description(
-    "Translate a text and put the result on the files in the output directory",
+    "Translate a text and put the result on the files in the output directory"
   )
   .option(
     "-t, --text <string>",
-    "The word or sentence that you want to translate.",
+    "The word or sentence that you want to translate."
   )
   .option(
     "-f, --from <string>",
-    "The language of the text that you wrote on the --text option.",
+    "The language of the text that you wrote on the --text option."
   )
   .option(
     "-n, --name <string>",
-    "The name of the property that you want your text has on the output files.",
+    "The name of the property that you want your text has on the output files."
   )
   .option(
     "-e, --engine <string>",
     `[OPTIONAL]. The engine that you want to use to translate the text. Available options: ${validEngines.join(", ")}` +
       "\nIf you specify a engine on the command, it will put it on the first position of the array of engines." +
       "\nIf for any reason the engine you selected is not available at that moment, then it will use the engines that you have defined on the configuration file in the priority order that you selected." +
-      "\nIf you don't specify any engine, the script will try to get your preferences from your configuration file, and if you don't have any configuration file, it will use by default all the translation engine that are free and doesn't need API Key.",
+      "\nIf you don't specify any engine, the script will try to get your preferences from your configuration file, and if you don't have any configuration file, it will use by default all the translation engine that are free and doesn't need API Key."
   )
   .option(
     "-s, --settings-file <string>",
     `[OPTIONAL]. Use this flag if you want to specify a different path for the configuration file that the default path.
     
     If you don't specify this flag, it'll search for a file called \`i18n-populator.config.json\` on the root of your project.`,
-    configPath,
+    configPath
   )
   .action(translateController);
 
@@ -51,9 +51,11 @@ program
   .description("Show the languages supported in ISO-639-1 standard")
   .option(
     "-e, --by-engine <string>",
-    "Filter the language supported list by engine",
+    "Filter the language supported list by engine"
   )
-  .action(languagesController);
+  .action(() => {
+    languagesController;
+  });
 
 program
   .command("init")
---
+++ src/utils/getOrCreateJsonFile.test.ts
@@ -0,0 +1,51 @@
+import { parsePath } from "./getConfigPath";
+import fs from "fs";
+import { getOrCreateJsonFile } from "./getOrCreateJsonFile";
+import { beforeEach, describe, expect, it, jest } from "@jest/globals";
+
+describe("getOrCreateJsonFile", () => {
+  let basePath, fileName, fileContent;
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetModules();
+
+    basePath = "test-configs/translations";
+    fileName = "en.json";
+    fileContent = { test: "test" };
+  });
+
+  describe("When requested file doesn't exists in the provided route", () => {
+    it("should create a new file and return the file and the parsedPath", async () => {
+      (fs.existsSync as jest.Mock).mockImplementation(() => false);
+
+      const { file, parsedPath } = await getOrCreateJsonFile(
+        basePath,
+        fileName
+      );
+      expect(file).toEqual({});
+      expect(parsedPath).toEqual(parsePath(`${basePath}/${fileName}`));
+      expect(fs.mkdirSync).toHaveBeenCalledWith(basePath, { recursive: true });
+      expect(fs.writeFileSync).toHaveBeenCalledWith(
+        parsedPath,
+        JSON.stringify(file, null, 2)
+      );
+    });
+  });
+  // TODO: Mock import function
+  describe.skip("When requested file exists in the provided route", () => {
+    it("should return the file and the parsedPath if file exists", async () => {
+      // importJSONFile.mockImplementation(() => fileContent);
+      (fs.existsSync as jest.Mock).mockImplementation(() => true);
+
+      const { file, parsedPath } = await getOrCreateJsonFile(
+        basePath,
+        fileName
+      );
+      expect(file).toEqual(fileContent);
+      expect(parsedPath).toEqual(parsePath(`${basePath}/${fileName}`));
+      expect(fs.mkdirSync).not.toHaveBeenCalled();
+      expect(fs.writeFileSync).not.toHaveBeenCalled();
+      expect(fs.existsSync).toHaveBeenCalledWith(parsedPath);
+    });
+  });
+});
