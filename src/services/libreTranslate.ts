c7e5e9e9f751efa6aed1f851d5f49fc864eec5d8
Heliomar Peña <penaheliomar@gmail.com>
2025-02-22 13:07:07 -0300

    fix: fixed translateController and libreTranslate

--- src/services/libreTranslate.ts
+++ src/services/libreTranslate.ts
@@ -1,16 +1,21 @@
 import fetch from "node-fetch";
-import { TranslateOptions, TranslateResult, TranslateText } from "./translate";
-import { TranslationEngines } from "../types/settings";
+import {
+  TranslateFn,
+  TranslateOptions,
+  TranslateResult,
+  TranslateText,
+} from "./translate.d";
+import { TranslationEngines } from "../types/settings.d";
 
 const defaultMirrors = [
   "https://translate.terraprint.co/translate",
   "https://trans.zillyhuhn.com/translate",
 ];
 
-const libreTranslate = async (
-  text: TranslateText,
-  { from, to, config = {} }: TranslateOptions & { config: Omit<TranslationEngines[2], 'name'> },
-): Promise<TranslateResult> => {
+const libreTranslate: TranslateFn<TranslationEngines[2]> = async (
+  text,
+  { from, to, config = {} },
+) => {
   const { mirrors = [] } = config;
   const allMirrors = mirrors.concat(defaultMirrors);
 
@@ -29,16 +34,21 @@ const libreTranslate = async (
 
       return { text: res.translatedText };
     } catch (err) {
-      console.log(
-        `Mirror failed: ${url} with the next error:\n\n> ${err.message}\n\nTrying with the next one...\n`,
-      );
+      if (err instanceof Error) {
+        console.log(
+          `Mirror failed: ${url} with the next error:\n\n> ${err.message}\n\nTrying with the next one...\n`,
+        );
+      }
     }
   }
 
   throw new Error("All libreTranslate mirrors failed. Please try again later.");
 };
 
-const translate = async (text, { from, to, config }) => {
+const translate: TranslateFn<TranslationEngines[2]> = async (
+  text,
+  { from, to, config },
+) => {
   const result = await libreTranslate(text, { from, to, config });
 
   return result;
